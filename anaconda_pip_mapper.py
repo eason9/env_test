4d44331b1cba513d51de58128fd771b794f01f89
Garrett <31139946+eason9@users.noreply.github.com>
2020-07-24 12:14:43 -0400

    Create anaconda_pip_mapper.py

---
+++ anaconda_pip_mapper.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 15:18:11 2020
+
+@author: Garrett
+# To get anaconda env use: conda list -e > requirements.txt
+# Note can only map envs within the same os system (e.g. can map windows package versions to linux package versions)
+"""
+#%% Packages & Data
+import re
+
+read = r"C:\Users\Garrett\requirements.txt"
+write = r"C:\Users\Garrett\requirements2.txt"
+#%%
+with open(read, 'r') as file:
+    data = file.read()
+    
+test = re.sub(r'[=]{1}[^=]*\n', '\n', data)
+test = re.sub(r'=', '==', test)
+
+with open(write, 'w') as file:
+    file.write(test)
